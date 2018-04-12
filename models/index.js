51736542ddbd68b15b59baeaea25247a5c4448e2
lamanda <guill29640@gmail.com>
2018-04-12 10:56:46 +0200

    Handle no mongodb service

--- models/index.js
+++ models/index.js
@@ -1,9 +1,14 @@
 const fs = require("fs")
 const path = require("path")
 const basename = path.basename(__filename)
+require("colors")
 
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost/crypto")
+mongoose.connect("mongodb://localhost/crypto").catch(err => {
+  console.log(err.message.red)
+  console.log("This program need a mongo database".yellow)
+  process.exit(1)
+})
 
 fs
   .readdirSync(__dirname)
